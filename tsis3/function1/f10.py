078090e8b4fa41b05bf154780ce5acefd535553a
DADIxodi <tipodosym@gmail.com>
2023-02-09 16:20:51 +0600

    changed file f10.py

--- tsis3/function1/f10.py
+++ tsis3/function1/f10.py
@@ -1,8 +1,8 @@
 def unique(nums):
     sorted(nums)
-    new = nums[0]
-    for i in range(1, nums):
-        if nums[i-1] == nums[i]:
+    new = [nums[0]]
+    for i in range(1, len(nums)):
+        if nums[i-1] != nums[i]:
             new += nums[i]
     return new
 
