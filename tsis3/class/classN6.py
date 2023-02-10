014cd85fae6c4c1f3f6138461b51e66c24a41c4a
Dosym <tipodosym@gmail.com>
2023-02-10 14:34:41 +0600

    more correctly classN6.py

--- tsis3/class/classN6.py
+++ tsis3/class/classN6.py
@@ -1,12 +1,4 @@
-def isprime(x):
-    if x == 0 or x == 1:
-        return False
-    for i in range(2, x):
-        if x % i == 0:
-            return False
-    return True
-nums = input().split()
-for i in range(0, len(nums)):
-    nums[i] = int(nums[i])
-prime_nums = list(filter(lambda x: isprime(x), nums))
-print(prime_nums)
+mynums = map(int, input().split())
+mynums = list(filter(lambda x: x != 0 and x != 1, mynums))
+isprime = list(filter(lambda x: all(map(lambda i: x % i != 0, range(2, int(x ** 0.5) + 1))), mynums))
+print(isprime)
